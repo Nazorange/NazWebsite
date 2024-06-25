e1da2a5c636fbf004153d50dcc009a908d080a3c
Nazorange <snkhalan+git@g.syr.edu>
2024-06-25 01:00:46 -0400

    contact page 2.0

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -153,9 +153,9 @@ const Contact = () => {
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
             <ul className="flex flex-col gap-10">
               {info.map((item, index) => (
-                <li key={index} className="flex items-center gap-6">
-                  <div className="w-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                    <div className="text-[28px]">{item.icon}</div>
+                <li key={index} className="flex items-center gap-6 ">
+                  <div className=" w-[52px] h-[52px] bg-[#27272c] text-accent rounded-md flex items-center justify-center xl:p-8 ">
+                    <div className="text-[28px]  ">{item.icon}</div>
                   </div>
                   <div className="flex-1">
                     <p className="text-white/60">{item.title}</p>
