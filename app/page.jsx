fcbf630337643bcacce6ca95e6f4719499bba0e3
Nazorange <snkhalan+git@g.syr.edu>
2024-06-25 08:10:50 -0400

    Added Resume

--- app/page.jsx
+++ app/page.jsx
@@ -68,7 +68,7 @@ const Home = () => {
                     size="lg"
                     className="uppercase flex items-center gap-2"
                   >
-                    <span>Download CV</span>
+                    <span> CV</span>
                     <FiDownload className="text-xl" />
                   </Button>
                 </a>
@@ -78,6 +78,16 @@ const Home = () => {
                     iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
                   />
                 </div>
+                <a href="/Resume.pdf" download>
+                  <Button
+                    variant="outline"
+                    size="lg"
+                    className="uppercase flex items-center gap-2"
+                  >
+                    <span>Resume</span>
+                    <FiDownload className="text-xl" />
+                  </Button>
+                </a>
               </div> 
             </div>
             {/* photo */}
